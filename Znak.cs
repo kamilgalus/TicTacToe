31e1bd5464d9dd8bb49ea926324d4e4b88ae7106
Galus <kamil.galus.pl@gmail.com>
2019-12-04 21:58:44 +0100

    class Znak

---
+++ Znak.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTac
+{
+    class Znak
+    {
+        private int miejsceZnaczka;
+        private char pionek;
+
+        public int WybierzMiejsce(char wzor)
+        {
+            Console.Write("\n"+"\tPodaj miejsce gdzie chcesz postawic {0} [1-9]:", wzor);
+            miejsceZnaczka = int.Parse(Console.ReadLine());
+            return miejsceZnaczka;
+        }
+
+        public void NadajWzorPionka(char wzor)
+        {
+            pionek = wzor;
+        }
+      
+    }
+}
