5c32c0ac9c284921b6f8bae3cbc1cc127887f050
Galus <kamil.galus.pl@gmail.com>
2019-12-04 21:58:38 +0100

    funcion Main

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TicTac
+{
+    class Program
+    {
+        static void Main()
+        {
+            Kratka k1 = new Kratka();
+            Operacje op1 = new Operacje();
+            bool czyWygrana;
+            int iterator = 0;
+            char wzorPionka;
+
+
+            do
+            {              
+                Console.Clear();
+                k1.NarysujKrate();
+                wzorPionka = op1.KogoKolej(iterator);
+                k1.DodajPionek(iterator, wzorPionka);
+                czyWygrana = op1.SprawdzenieWygranej(k1.ZwrocKratke(), wzorPionka);
+                iterator++;
+            }
+            while (czyWygrana == false);
+            {
+                Console.Clear();
+                k1.NarysujKrate();
+                op1.KomunikatWygranej(wzorPionka);
+            }
+                
+            
+            
+        }
+    }
+}
